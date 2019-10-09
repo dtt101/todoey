153d3eff992eaa8f651629e8d7c53b720df1f007
David Thompson <dtt101@gmail.com>
2019-10-09 16:03:49 +0100

    Initial layout complete

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -5,42 +5,63 @@ class TasksScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body: Container(
-        padding: EdgeInsets.only(
-          left: 30,
-          top: 60,
-          right: 30,
-          bottom: 30,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            CircleAvatar(
-              backgroundColor: Colors.white,
-              child: Icon(
-                Icons.list,
-                size: 30,
-                color: Colors.lightBlueAccent,
-              ),
-              radius: 30,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(
+              left: 30,
+              top: 60,
+              right: 30,
+              bottom: 30,
             ),
-            SizedBox(
-              height: 10,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Icon(
+                    Icons.list,
+                    size: 30,
+                    color: Colors.lightBlueAccent,
+                  ),
+                  radius: 30,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Todoey',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                  '13 Tasks',
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              ],
             ),
-            Text(
-              'Todoey',
-              style: TextStyle(
+          ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
                 color: Colors.white,
-                fontSize: 50,
-                fontWeight: FontWeight.w700,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
               ),
             ),
-            Text(
-              '13 Tasks',
-              style: TextStyle(color: Colors.white, fontSize: 18),
-            ),
-          ],
-        ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        backgroundColor: Colors.lightBlueAccent,
+        child: Icon(Icons.add),
       ),
     );
   }
