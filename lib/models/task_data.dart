458216b9cb24d1693c0d9d9af6a03b7f43c34b86
David Thompson <dtt101@gmail.com>
2019-12-02 21:57:16 +0000

    Add unmodifiable getter

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -1,3 +1,4 @@
+import 'dart:collection';
 import 'package:flutter/foundation.dart';
 import 'package:todoey/models/task.dart';
 
@@ -7,6 +8,10 @@ class TaskData extends ChangeNotifier {
     Task(name: 'Task 2'),
     Task(name: 'Task 3'),
   ];
+
+  UnmodifiableListView<Task> get tasks {
+    return UnmodifiableListView(_tasks);
+  }
 
   int get taskCount {
     return _tasks.length;
