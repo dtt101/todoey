8c4d37297fa4fdbcc866acaf37d6e319331ac32a
David Thompson <dtt101@gmail.com>
2019-12-02 21:53:14 +0000

    Make tasks private

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -2,18 +2,18 @@ import 'package:flutter/foundation.dart';
 import 'package:todoey/models/task.dart';
 
 class TaskData extends ChangeNotifier {
-  List<Task> tasks = [
+  List<Task> _tasks = [
     Task(name: 'Task a'),
     Task(name: 'Task 2'),
     Task(name: 'Task 3'),
   ];
 
   int get taskCount {
-    return tasks.length;
+    return _tasks.length;
   }
 
   void addTask(String newTaskTitle) {
-    tasks.add(Task(name: newTaskTitle));
+    _tasks.add(Task(name: newTaskTitle));
     notifyListeners();
   }
 }
